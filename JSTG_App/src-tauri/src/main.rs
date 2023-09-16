271237d22eb216308d9f12290a2334bdd7d959a1
Ben Saunders-Henning <ben.shp@proton.me>
2023-09-16 18:20:06 -0400

    Added the SQLite package along with a test function.

--- JSTG_App/src-tauri/src/main.rs
+++ JSTG_App/src-tauri/src/main.rs
@@ -2,10 +2,14 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+  println!("Trying the DB function:\n\n");
+  test();
+
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![double, greet])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
 }
 
 #[tauri::command]
@@ -16,4 +20,23 @@ fn greet(name: &str) -> String {
 #[tauri::command]
 fn double(count: i32) -> i32 {
     count * 2
+}
+
+fn test() {
+
+    let connection = sqlite::open("/home/ben/projects/JSTG/JSOTdb").unwrap();
+
+    let query = "
+        SELECT * FROM [Assessors];
+    ";
+
+    connection
+        .iterate(query, |pairs| {
+            for &(name, value) in pairs.iter() {
+                println!("{} = {}", name, value.unwrap());
+            }
+            true
+        })
+        .unwrap();
+
 }
