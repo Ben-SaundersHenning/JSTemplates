cc75e9559981565e726e26ee87441545424a7424
Ben Saunders-Henning <ben.shp@proton.me>
2024-05-18 14:53:14 -0400

    Added some base files for UI.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from "vue";
-import "./styles.css";
-import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router"
+import "./styles.scss";
+import App from "./App.vue"
+import Home from "./views/Home.vue"
 
-createApp(App).mount("#app");
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+        // {
+        //     path: '/about',
+        //     component: () => import("./views/About.vue")
+        // }
+    ]})
+
+const app = createApp(App)
+
+app.use(router).mount("#app");
