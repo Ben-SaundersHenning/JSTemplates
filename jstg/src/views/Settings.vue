fd267d081488b6c92066b76371adb6e1b3f5fc59
Ben Saunders-Henning <ben.shp@proton.me>
2025-01-18 14:37:25 -0500

    Added vee-validate form to settings page

--- jstg/src/views/Settings.vue
+++ jstg/src/views/Settings.vue
@@ -1,5 +1,35 @@
 <script lang="ts" setup>
 
+    import { onMounted } from "vue";
+    import { useForm } from "vee-validate";
+    import { toTypedSchema } from "@vee-validate/zod";
+    import { z } from "zod";
+
+    const { errors, handleSubmit, defineField } = useForm({
+        validationSchema: toTypedSchema(z.object({
+            savePath: z.string().trim().min(1), //TODO validate directory
+        }))
+    });
+
+    const [savePath, savePathAtrb] = defineField("savePath");
+
+    const onSubmit = handleSubmit(onSuccess, onInvalidSubmit);
+
+    function onSuccess(values) {
+
+    }
+
+    function onInvalidSubmit({ values, errors, results }) {
+        console.log(errors);
+    }
+
+    onMounted(() => {
+
+        //get config
+        //apply config to fields
+
+    })
+
 </script>
 
 <template>
@@ -8,9 +38,8 @@
         <div class="inputs">
             <div class="path-input vertical-input">
                 <p class="input-label">Document Save Path</p>
-                <input aria-label="Document Save Path" id="savepath-input" class="input-border" type="text" name="savepath"/>
-                <!-- <input aria-label="Document Save Path" id="savepath-input" class="input-border" type="text" name="savepath" v-model="" :=""/> -->
-                <!-- <span class="error">{{errors['claimant.firstName']}}</span> -->
+                <input aria-label="Document Save Path" id="savepath-input" class="input-border" type="text" name="savepath" v-model="savePath" :="savePathAtrb"/>
+                <span class="error">{{errors['savePath']}}</span>
             </div>
         </div>
         <div class="horizontal-input" style="justify-content: center; margin-top: 30px;">
